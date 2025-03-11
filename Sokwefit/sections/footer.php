a4b71539d761758b8186d483bc9d68617004fd11
mudathir abdallah <mudathirrajab69@gmail.com>
2025-03-11 12:22:49 -0700

    footer_updates

--- Sokwefit/sections/footer.php
+++ Sokwefit/sections/footer.php
@@ -4,12 +4,12 @@
 
         <div class="box">
             <h3>Our Products</h3>
-            <a href="#">Gym Towel</a>
-            <a href="#">Pullover</a>
-            <a href="#">Sockd</a>
-            <a href="#">Vest</a>
-            <a href="#">Mugs</a>
-            <a href="#">Bags</a>
+            <a href="#menu">Gym Towel</a>
+            <a href="#menu">Pullover</a>
+            <a href="#menu">Socks</a>
+            <a href="#menu">Vest</a>
+            <a href="#menu">Mugs</a>
+            <a href="#menu">Bags</a>
         </div>
 
       
@@ -17,20 +17,19 @@
         <div class="box">
             <h3>contact info</h3>
             <p>+255 743 536 373</p>
-            <p>+255 656 722 784</p>
             <p>info@sokwefit.com</p>
-           
+            <p>Dar-Es-Salaam, Tanzania</p>
         </div>
 
         <div class="box">
             <h3>follow me</h3>
             <a href="https://www.facebook.com/people/SOKWE-Fitness/61566860461102/">facebook</a>
-            <a href="#">twitter</a>
             <a href="https://www.instagram.com/sokwefitness/">instagram</a>
+            <a href="https://www.youtube.com/@abuujr87/featured">YouTube</a>
         </div>
 
     </div>
 
-    <div class="credit"> copyright @ 2025 by <span>Sokwefitness.</span> </div>
+    <div class="credit"><p class="credit">&copy; 2024 SokweFitness. All rights reserved.</p> </div>
 
 </section>
